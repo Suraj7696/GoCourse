2dd00807f82ff8b63d14aa3acfac1bceb5d2ad1f
suraj7696 <satmakur@asu.edu>
2023-11-07 14:34:26 -0500

    Assignment-3 done

--- myproject/main.go
+++ myproject/main.go
@@ -1,18 +1,25 @@
 package main
 
-import (
-	"fmt"
-	controlstructures "myproject/controlStructures"
-	moreTypes "myproject/moretypes"
-)
+import assignment_3 "myproject/assignment_3"
 
 func main() {
-	fmt.Println("Hello world!")
-	controlstructures.PrintName10Times()
-	fmt.Println(controlstructures.IsEven(10))
-	controlstructures.SwitchNames(5)
-	controlstructures.DeferStatements()
-	moreTypes.Arrays()
-	moreTypes.Pointers()
-	moreTypes.Library()
+	var numbers []int
+	assignment_3.PrintSlice(numbers)
+
+	numbers = append(numbers, 0)
+	assignment_3.PrintSlice(numbers)
+
+	numbers = append(numbers, 1)
+	assignment_3.PrintSlice(numbers)
+
+	numbers = append(numbers, 2, 3, 4)
+	assignment_3.PrintSlice(numbers)
+
+	numbers1 := make([]int, len(numbers), (cap(numbers))*2)
+
+	copy(numbers1, numbers)
+	assignment_3.PrintSlice(numbers1)
+
+	assignment_3.GoMap()
+
 }
